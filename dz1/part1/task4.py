84a8ef1cf355167cbceb667fc3e981069aea3728
Julia Tsyganok <julia.tsyganok.9@yandex.ru>
2026-03-21 13:55:31 +0300

    hhh

--- dz1/part1/task4.py
+++ dz1/part1/task4.py
@@ -1,4 +1,4 @@
-from part1.class_polynom import Polynom
+from class_polynom import Polynom
 
 def limit_infty(f, g):
     df = f.deg()
@@ -9,8 +9,26 @@ def limit_infty(f, g):
         return f.coef[-1] / g.coef[-1]
     return float("inf")
 
+def limit_at_A(f, g, A):
+    f_val = f(A) 
+    g_val = g(A)
+    if abs(g_val) > 1e-9:
+        return f_val / g_val
+    if abs(f_val) < 1e-9:
+        f_reduced = gorner(f, A)
+        g_reduced = gorner(g, A)
+        return limit_at_A(f_reduced, g_reduced, A)  
+    return float("inf")
 
-f = Polynom([1,0,1])
-g = Polynom([1,1])
 
-print(limit_infty(f, g))
+def gorner(p, A):
+    coeffs = p.coef[::-1]
+    result = []
+    rem = 0
+    for c in coeffs:
+        rem = rem * A + c 
+        result.append(rem)
+
+    result.pop()       
+    result = result[::-1] 
+    return Polynom(result)
